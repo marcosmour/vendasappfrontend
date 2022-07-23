b122cc4c131ebb160bb779819201663f8a6ad7ad
Marcos Moura <marcosmoura201@hotmail.com>
2022-07-23 15:54:08 -0300

    Manipulacao de formulario com state

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,6 +1,23 @@
+import { useState} from 'react'
 import { Layout } from 'components'
 
 export const CadastroProdutos: React.FC = () =>{
+
+    const [sku, setSku] = useState('');
+    const [preco, setPreco] = useState('');
+    const [nome, setNome] = useState('');
+    const [descricao, setDescricao] = useState('');
+
+    const submit = () => {
+        const produto = {
+            sku, 
+            preco, 
+            nome, 
+            descricao
+        }
+        console.log(produto)
+    }
+
     return (
         <Layout titulo='Produtos'>
             <div className="columns">
@@ -8,7 +25,8 @@ export const CadastroProdutos: React.FC = () =>{
                     <label className="label" htmlFor="inputSku">SKU: *</label>
                     <div className="control">
                         <input className="input"
-                            id="inputSku"
+                            id="inputSku" value={sku}
+                            onChange={ event=>setSku(event.target.value)}
                             placeholder="Digite o SKU do produto" />
 
                     </div>
@@ -18,7 +36,8 @@ export const CadastroProdutos: React.FC = () =>{
                     <label className="label" htmlFor="inputPreco">Preço: *</label>
                     <div className="control">
                         <input className="input"
-                            id="inputPreco"
+                            id="inputPreco" value={preco}
+                            onChange={ event=>setPreco(event.target.value)}
                             placeholder="Digite o Preço do produto" />
 
                     </div>
@@ -29,7 +48,8 @@ export const CadastroProdutos: React.FC = () =>{
                     <label className="label" htmlFor="inputNome">Nome: *</label>
                     <div className="control">
                         <input className="input"
-                            id="inputNome"
+                            id="inputNome" value={nome}
+                            onChange={ event=>setNome(event.target.value)}
                             placeholder="Digite o Nome do produto" />
 
                     </div>
@@ -41,7 +61,8 @@ export const CadastroProdutos: React.FC = () =>{
                     <label className="label" htmlFor="inputDesc">Descrição: *</label>
                     <div className="control">
                         <textarea className="textarea"
-                            id="inputDesc"
+                            id="inputDesc" value={descricao}
+                            onChange={ event=>setDescricao(event.target.value)}
                             placeholder="Digite a Descrição detalhada do produto" />
 
                     </div>
@@ -50,7 +71,7 @@ export const CadastroProdutos: React.FC = () =>{
 
             <div className="field is-grouped">
                 <div className="control">
-                     <button className="button is-link">Submit</button>
+                     <button onClick={submit} className="button is-link">Submit</button>
                          </div>
                           <div className="control">
                   <button className="button is-link is-light">Cancel</button>
