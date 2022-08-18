14072eb1f2e40450631b08daa9b87435bcb67c1d
Marcos Moura <marcosmoura201@hotmail.com>
2022-08-18 17:04:49 -0300

    Componente de Tabela

--- src/components/produtos/listagem/index.tsx
+++ src/components/produtos/listagem/index.tsx
@@ -1,13 +1,31 @@
 import { Layout} from 'components'
 import Link from 'next/link'
+import { TabelaProdutos} from './tabela'
+import{ Produto} from 'app/models/produtos'
 
 export const ListagemProdutos: React.FC = () => {
+
+    const produto: Produto[] = [{
+        id: "1", sku: "HGDGD", nome: "Impressora", preco: 2500
+    },
+    {
+        id: "1", sku: "HGDGD", nome: "Impressora", preco: 2500
+    },
+
+    {
+        id: "1", sku: "HGDGD", nome: "Impressora", preco: 2500
+    },
+    {
+        id: "1", sku: "HGDGD", nome: "Impressora", preco: 2500
+    }]
+
     return (
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
             <button className="button is-warning">Novo</button>
             </Link>
             <br />
+            <TabelaProdutos produtos={produto} />
         </Layout>
     )
 }
---
+++ src/components/produtos/listagem/tabela/index.tsx
@@ -0,0 +1,46 @@
+import {Produto } from 'app/models/produtos'
+
+interface TabelaProdutosProps{
+    produto: Array<Produto>;
+}
+
+export const TabelaProdutos: React.FC<TabelaProdutosProps> = ({
+    produtos
+}) => {
+    return (
+        <table className="table is-striped is-hoverable">
+            <thead>
+                <th>Código</th>
+                <th>SKU</th>
+                <th>Nome</th>
+                <th>Preço</th>
+            </thead>
+            <tbody>
+                {
+                    produtos.map( produto => <ProdutoRow key={produto.id} produto={produto}/>)
+                }
+            </tbody>
+        </table>
+    )
+}
+
+interface ProdutoRowProps {
+    produto: Produto;
+}
+
+const ProdutoRow: React.FC<ProdutoRowProps> = ({
+    produto
+}) => {
+    return (
+        <tr>
+            <td>{ produto.id}</td>
+            <td>{ produto.sku}</td>
+            <td>{ produto.nome}</td>
+            <td>{ produto.preco}</td>
+            <td>
+                <button className="button is-success">Editar</button>
+                <button className="button is-danger">Deletar</button>
+            </td>
+        </tr>
+    )
+}
