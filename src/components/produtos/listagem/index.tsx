b636aaaa3bab80841cdd6610052ef4f92544cad4
Marcos Moura <marcosmoura201@hotmail.com>
2022-08-24 19:43:29 -0300

    Criando um loader com Pure CSS Loader

--- src/components/produtos/listagem/index.tsx
+++ src/components/produtos/listagem/index.tsx
@@ -1,4 +1,4 @@
-import { Layout} from 'components'
+import { Layout, Loader} from 'components'
 import Link from 'next/link'
 import { TabelaProdutos} from './tabela'
 import{ Produto} from 'app/models/produtos'
@@ -12,20 +12,15 @@ export const ListagemProdutos: React.FC = () => {
     const { data: result, error} = useSWR<AxiosResponse<Produto[]>>
                     ('/api/produtos', url => httClient.get(url))
 
-    if(!result){
-        return(
-            <div>Carregando</div>
-        )
-
-    }
-
+ 
     return (
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
             <button className="button is-warning">Novo</button>
             </Link>
             <br />
-            <TabelaProdutos produtos={result?.data} />
+            <Loader show={!result}/>
+            <TabelaProdutos produtos={result?.data || []} />
         </Layout>
     )
 }
