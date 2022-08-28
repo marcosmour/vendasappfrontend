a7ce304d49bcacd021c1fb74f0db084062335b94
Marcos Moura <marcosmoura201@hotmail.com>
2022-08-28 13:29:51 -0300

    Editar e excluir produtos

--- src/components/produtos/listagem/index.tsx
+++ src/components/produtos/listagem/index.tsx
@@ -12,15 +12,23 @@ export const ListagemProdutos: React.FC = () => {
     const { data: result, error} = useSWR<AxiosResponse<Produto[]>>
                     ('/api/produtos', url => httClient.get(url))
 
+    const editar = (produto: Produto) => {
+        console.log(produto)
+    }
+
+    const deletar = (produto: Produto) => {
+        console.log(produto)
+    }
+
  
     return (
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
             <button className="button is-warning">Novo</button>
             </Link>
-            <br />
+            <br /> <br />
             <Loader show={!result}/>
-            <TabelaProdutos produtos={result?.data || []} />
+            <TabelaProdutos onEdit={editar} onDelete={deletar} produtos={result?.data || []} />
         </Layout>
     )
 }
